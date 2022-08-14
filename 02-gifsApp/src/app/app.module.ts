2753466fa64555b0d9af0707a5fb89b0ff463d38
dinogs <dino.garcia.s@gmail.com>
2022-08-14 17:20:43 -0400

    actualiza curso

--- 02-gifsApp/src/app/app.module.ts
+++ 02-gifsApp/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { heroesModule } from './heroes/heroes.module';
-import { ContadorModule } from './contador/contador.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule,
-    heroesModule,
-    ContadorModule
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
